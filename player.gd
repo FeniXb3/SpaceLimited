c9021064c2de4414c969044526c7a33fc7691762
Konrad Gadzina <konrad@makegames.today>
2023-10-19 16:08:12 +0200

    Update player movement

--- player.gd
+++ player.gd
@@ -19,16 +19,19 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	direction = Input.get_axis("ui_up", "ui_down")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	var 
+#
+#	direction = Input.get_axis("ui_up", "ui_down")
+#	if direction:
+#		velocity.y = direction * SPEED
+#	else:
+#		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED
 	move_and_slide()
